f43e8bd1346040a22e9693b4a5366f88572a4fdc
edueric-hphk <edueric.hphk@gmail.com>
2019-11-05 17:01:31 +0900

    191105| django resizing images & favicon

--- django_statics/articles/models.py
+++ django_statics/articles/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
-def articles_image_path(instance, filename):
-    # return f'articles/{instance.pk}번글/images/{filename}'
-    # /media/articles/2번글/images/baby.jpg
-    return 'articles/%Y/%m/%d/'
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 # Create your models here.
 class Article(models.Model):
@@ -11,4 +8,10 @@ class Article(models.Model):
     content = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(blank=True, upload_to='articles/%Y/%m/%d') # ImageField는 pillow 필수
+    # image = models.ImageField(blank=True, upload_to='articles/%Y/%m/%d') # ImageField는 pillow 필수
+    image = ProcessedImageField(
+        processors=[Thumbnail(200, 300)], # 처리할 작업 목록
+        format='JPEG', # 저장 포맷
+        options={'quality': 90}, # 추가 옵션
+        upload_to='articles/images' # 저장 위치 (MEDIA_ROOT/articles/images)
+    )
